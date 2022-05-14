982a1e30a59f3508895cdb8ab1689d297578eaf9
Joel M. Boada V <jmboada30@gmail.com>
2022-05-14 12:42:08 -0400

    se agregan component

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Quien es este Pokemon?</h1>
+
+  <pokemon-picture></pokemon-picture>
+  <pokemon-options></pokemon-options>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+export default {
+  components: { PokemonPicture, PokemonOptions },
+
+}
+</script>
+
+<style>
+
+</style>
