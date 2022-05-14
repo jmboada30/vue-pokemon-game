3ddc47e55b288209218b3b058401ff6805f6f2b2
Joel M. Boada V <jmboada30@gmail.com>
2022-05-14 16:59:58 -0400

    solo falta reiniciar el juego ya da respuesta al seleccionar una opcion

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,8 +1,14 @@
 <template>
-  <h1>Quien es este Pokemon?</h1>
+  <h1 v-if="!pokemon">Por favor espere...</h1>
 
-  <pokemon-picture :pokemonId="4" :showPokemon="false"></pokemon-picture>
-  <pokemon-options></pokemon-options>
+  <div v-else>
+    <h1>Quien es este Pokemon?</h1>
+    <pokemon-picture
+      :pokemonId="pokemon.id"
+      :showPokemon="showPokemon"
+    ></pokemon-picture>
+    <pokemon-options :pokemons="pokemonArr" @selection="checkAnswer"></pokemon-options>
+  </div>
 </template>
 
 <script>
@@ -11,6 +17,34 @@ import PokemonOptions from "@/components/PokemonOptions.vue";
 import getPokemonOptions from "@/helpers/getPokemonOptions";
 export default {
   components: { PokemonPicture, PokemonOptions },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+      this.selectPokemon();
+    },
+    selectPokemon() {
+      const index = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[index];
+    },
+    checkAnswer(pokemonId) {
+      this.showPokemon = true;
+      if(pokemonId === this.pokemon.id) {
+        alert('YES')
+      } else {
+        alert('NOO')
+      }
+    }
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
 };
 </script>
 
